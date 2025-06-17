47c4c966b5c4054b5a209427e691d5f118b21e86
AweAwe95 <vitalikarach95@gmail.com>
2025-06-17 16:10:06 +0200

    Deployed 0ec6722 with MkDocs version: 1.6.1

---
+++ javascript/widget.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var script = document.createElement("script");
+  script.src = "https://main.d1hlqxzsj0hfwo.amplifyapp.com/widget-import.js";
+  script.setAttribute("data-entity-id", "DQ2p1J4W");
+  script.setAttribute("data-sample-questions", '["What is this widget?", "How to get started?"]');
+  script.setAttribute("data-community-name", "ExampleCommunity");
+  script.async = true;
+  document.head.appendChild(script);
+});
